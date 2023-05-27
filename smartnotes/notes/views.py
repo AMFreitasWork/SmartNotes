2e9fd1457f19a566154e2581a63ba73fde473ed4
Andre Freitas <andre.freitas.95.work@gmail.com>
2023-05-27 15:56:47 +0100

    Editado por André
    CRUD FUll Implemented

--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.http import Http404
 from .models import Note
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 from .forms import NotesForm
 
 # Create your views here.
+
+class NotesDeleteView(DeleteView):
+    model = Note
+    success_url ='/smart/notes'
+    template_name = 'notes/notes_delete.html'
 
 class NotesUpdateView(UpdateView):
     model= Note
