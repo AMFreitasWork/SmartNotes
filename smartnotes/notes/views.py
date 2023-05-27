aeb90dc63217d681fd83f904055d213f3a960e3f
Andre Freitas <andre.freitas.95.work@gmail.com>
2023-05-27 17:10:07 +0100

    Editado por André
    CRUD FUll Implemented

--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,8 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from .models import Note
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import NotesForm
 
 # Create your views here.
@@ -18,16 +22,29 @@ class NotesUpdateView(UpdateView):
     success_url = '/smart/notes'
     template_name = 'notes/notes_form.html'
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Note
+    success_url = '/smart/notes' 
     form_class = NotesForm
-    success_url = '/smart/notes'
+    login_url= "/admin"   
     template_name = 'notes/notes_form.html'
+            
+    def form_valid(self, form):
+        self.object= form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect (self.get_success_url())
     
-class NotesListView(ListView):
+    
+    
+class NotesListView(LoginRequiredMixin, ListView):
     model = Note
     context_object_name =  "notes"
     template_name = 'notes/notes_list.html'
+    login_url="/admin"
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
     
     
 class NotesDetailView(DetailView):
