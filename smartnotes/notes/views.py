6b2e9db5a88d0112899fedd4bebff4adecee43a9
Andre Freitas <andre.freitas.95.work@gmail.com>
2023-05-27 15:41:16 +0100

    Editado por André
    Nova Feature do Site

--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import Note
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from .forms import NotesForm
 
 # Create your views here.
 
+class NotesUpdateView(UpdateView):
+    model= Note
+    form_class = NotesForm
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_form.html'
+
 class NotesCreateView(CreateView):
     model = Note
     form_class = NotesForm
+    success_url = '/smart/notes'
     template_name = 'notes/notes_form.html'
     
 class NotesListView(ListView):
@@ -21,6 +28,7 @@ class NotesDetailView(DetailView):
     model = Note
     context_object_name =  "note"
     template_name = 'notes/notes_detail.html'
+
 
 
     
